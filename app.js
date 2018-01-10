ae806f4eecdeab5db2213243e10110cb04562772
faklijta <faklijta@gmail.com>
2018-01-10 16:31:22 +0100

    feat(eventlistener) added

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function updateTimerDisplay(){
     // Update current time text display.
     $('#current-time').text(formatTime( player.getCurrentTime() ));
     $('#duration').text(formatTime( player.getDuration() ));
-}
+};
 
 function formatTime(time){
     time = Math.round(time);
@@ -49,4 +49,63 @@ function formatTime(time){
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
     return minutes + ":" + seconds;
+};
+
+let s = {
+
+	init: function() {
+		$('.star').click(function(e) {
+			$("#" + e.currentTarget.id).toggleClass('selected');
+			if(s.stickers[e.currentTarget.id]) {
+				delete s.stickers[e.currentTarget.id];
+			} else {
+				s.stickers[e.currentTarget.id] = true;
+			}
+            s.save(true);
+        });
+
+		$('body').click(function(e) {
+			$('#ship').css({top:e.clientY, left: e.clientX});
+        });
+
+		s.load(true)
+	},
+
+	stickers: {},
+
+	save: function(local) {
+	    if (local) {
+	        localStorage.stickers = JSON.stringify(s.stickers);
+	        localStorage.fedexpottytrainer = localStorage.fedexpottytrainer;
+		}
+	},
+
+	load: function(local) {
+		$('.stickers').removeClass('selected');
+		if(local) {
+			if(localStorage.stickers === undefined) {
+				s.reset(true);
+			}
+			if(localStorage.fedexpottytrainer === undefined | localStorage.fedexpottytrainer == "") {
+				localStorage.fedexpottytrainer = "My potty chart";
+			}
+			s.stickers = JSON.parse(localStorage.stickers);
+		}
+		for(var sticker in s.stickers) {
+			$('#' + sticker).addClass('selected');
+		}
+	},
+
+	reset: function(local) {
+		if(local) {
+		    s.stickers = {star01:true};
+		    localStorage.fedexpottytrainer = "My potty chart";
+		    s.save(true);
+		}
+		s.load(true);
+	}
+
+
 }
+
+$(s.init);
